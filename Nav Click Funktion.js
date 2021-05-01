da307625071ac207966c35fb86b125386b869bd4
Markus Wohlgenannt <markus.wohlgenannt@hotmail.com>
2021-05-02 00:44:29 +0200

    02,05,21 b

---
+++ Nav Click Funktion.js
@@ -0,0 +1,66 @@
+  
+// Nav Click Funktion 
+function nav_home_click(){
+    $(Nav_Home).addClass('active');
+    $(Nav_Home).siblings().removeClass('active');
+}
+
+function nav_gelb_click(){
+    $(Nav_Gelb).addClass('active');
+    $(Nav_Gelb).siblings().removeClass('active');
+}
+
+function nav_weiss_click(){
+    $(Nav_Weiss).addClass('active');
+    $(Nav_Weiss).siblings().removeClass('active');
+}
+
+function nav_grün_click(){
+    $(Nav_Grün).addClass('active');
+    $(Nav_Grün).siblings().removeClass('active');
+}
+
+function nav_blau_click(){
+    $(Nav_Blau).addClass('active');
+    $(Nav_Blau).siblings().removeClass('active');
+}
+
+function nav_violett_click(){
+    $(Nav_Violett).addClass('active');
+    $(Nav_Violett).siblings().removeClass('active');
+}
+
+function nav_rot_click(){
+    $(Nav_Rot).addClass('active');
+    $(Nav_Rot).siblings().removeClass('active');
+}
+
+function nav_orange_click(){
+    $(Nav_Orange).addClass('active');
+    $(Nav_Orange).siblings().removeClass('active');
+}
+function nav_rosa_click(){
+    $(Nav_Rosa).addClass('active');
+    $(Nav_Rosa).siblings().removeClass('active');
+}
+
+
+
+
+
+
+
+  
+  /*
+
+  var hidden = false;
+    function action() {
+        hidden = !hidden;
+        if(hidden) {
+            document.getElementById('togglee').style.visibility = 'hidden';
+        } else {
+            document.getElementById('togglee').style.visibility = 'visible';
+        }
+    }
+
+    */

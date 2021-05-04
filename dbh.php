aa8473dda2efd9c367f1b36c545729a353fbc396
Markus Wohlgenannt <markus.wohlgenannt@hotmail.com>
2021-05-04 21:33:41 +0200

    04.05.21 DB connection wörks no display

--- dbh.php
+++ dbh.php
@@ -7,4 +7,8 @@ $dbname = "ajax";
 
 $conn = mysqli_connect($servername, $username, $passwort, $dbname); 
 
-?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  echo "Connected successfully";
+  ?>

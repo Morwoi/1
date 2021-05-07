2d6efa43039eb9551ee7f0b392bef421f9aa0443
Markus Wohlgenannt <markus.wohlgenannt@hotmail.com>
2021-05-08 00:44:08 +0200

    08.05.21 b

--- Databese5.php
+++ Databese5.php
@@ -1,10 +1,14 @@
 <?php
-include 'dbh.php';
+include 'db.php';
 $sql = "SELECT * FROM comments";
 $result = mysqli_query($conn, $sql);
 
 while($row=mysqli_fetch_array($result))
 {
 	echo $row['author']." ".$row['message']."<br>";
+	
 }
+
+
+
 ?>
